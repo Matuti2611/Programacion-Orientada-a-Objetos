43934b81acc6e424a15030adb579bcbc4a7d2b51
Matias <matuti2611@gmail.com>
2026-04-21 14:55:31 -0300

    refino el semaforo y hago el cronometro

---
+++ Cronometro/Cronometro/Cronometro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SimuladorCronometro
+{
+    public class Cronometro
+    {
+        private int minutos;
+        private int segundos;
+
+        public Cronometro()
+        {
+            minutos = 0;
+            segundos = 0;
+        }
+
+        public void reiniciar()
+        {
+            minutos = 0;
+            segundos = 0;
+        }
+
+        public void incrementarTiempo()
+        {
+            segundos++;
+
+            if (segundos > 59)
+            {
+                minutos++;
+                segundos = 0;
+            }
+        }
+
+        public string mostrarTiempo()
+        {
+            return $"{minutos} minutos, {segundos} segundos";
+        }
+    }
+}
---
+++ Cronometro/Cronometro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimuladorCronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cronometro cronometro = new Cronometro();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                cronometro.incrementarTiempo();
+            }
+
+            Console.WriteLine(cronometro.mostrarTiempo());
+            Console.ReadLine();
+        }
+    }
+}
